8a2f35fa5024440b93d63fb683ded064ade332de
vetterocki <92949251+vetterocki@users.noreply.github.com>
2022-05-08 23:30:24 +0300

    Add files via upload

---
+++ course/spring_2 (course)/src/main/java/com/example/cinema/repository/SeatRepository.java
@@ -0,0 +1,9 @@
+package com.example.cinema.repository;
+/*
+import com.example.cinema.entity.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+}*/
