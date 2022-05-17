5fb12114b6efb17250d7fc2acbf856f4e85a8c72
vetterocki <92949251+vetterocki@users.noreply.github.com>
2022-05-17 22:28:49 +0300

    Delete course/spring_2 (course) directory

--- course/spring_2 (course)/src/main/java/com/example/cinema/exceptions/custom/InvalidParamsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.cinema.exceptions.custom;
-
-public class InvalidParamsException extends RuntimeException {
-    public InvalidParamsException(String username, String password) {
-        super("Invalid params: " + username + " or " + password);
-    }
-
-}
--- course/spring_2 (course)/src/main/java/com/example/cinema/repository/SeatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cinema.repository;
-/*
-import com.example.cinema.entity.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SeatRepository extends JpaRepository<Seat, Long> {
-}*/
