d486a83e176b6d6a052db92c461a1ad54054207d
vetterocki <92949251+vetterocki@users.noreply.github.com>
2022-06-06 01:54:08 +0300

    Delete example/src directory

--- example/src/main/java/com/example/learn/EntitRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.learn;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EntitRepository extends JpaRepository<Example, Long> {
-}
